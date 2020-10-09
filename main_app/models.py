6a268bbc2a722e839d77bfa69618a42eb5fc5ef4
kkonkel23 <kailahkonkel@gmail.com>
2020-10-09 14:56:17 -0400

    add activities page

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Activity(models.Model):
+    name = models.CharField(max_length=250)
+    savings = models.IntegerField()
+    about = models.CharField(max_length=250)
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
+from .models import Activity
+from django.views.generic import ListView
 
 # Create your views here.
-
 def home(request):
   return render(request, 'home.html')
 
 def facts(request):
   return render(request, 'facts.html')
+
+class ActivityList(ListView):
+    model = Activity
