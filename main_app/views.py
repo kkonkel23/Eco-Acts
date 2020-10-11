66c32d212d64f2b6825fe016b7e6eefbf14140c0
kkonkel23 <kailahkonkel@gmail.com>
2020-10-11 18:49:28 -0400

    add signup

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Activity, MyActivity
 from django.views.generic import ListView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def home(request):
@@ -8,6 +10,25 @@ def home(request):
 
 def facts(request):
   return render(request, 'facts.html')
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    # This is how to create a 'user' form object
+    # that includes the data from the browser
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      # This will add the user to the database
+      user = form.save()
+      # This is how we log a user in via code
+      login(request, user)
+      return redirect('index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  # A bad POST or a GET request, so render signup.html with an empty form
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
 
 # def my_activites_index(request, activity_id):
 #     activity = Activity.objects.get(id=activity_id)
