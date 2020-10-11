264703ca13c433e9948c21b97a8eedce705c954c
kkonkel23 <kailahkonkel@gmail.com>
2020-10-11 18:38:36 -0400

    add login page

--- main_app/models.py
+++ main_app/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-# from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 
 # Create your models here.
 class MyActivity(models.Model):
     name = models.CharField(max_length=250)
     savings = models.IntegerField()
     about = models.CharField(max_length=250)
-    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     # def __str__(self):
     #     return self.name
