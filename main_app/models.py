a2e55198973d4d4cb0fdb1fdeefadee2eebad364
kkonkel23 <kailahkonkel@gmail.com>
2020-10-13 13:09:34 -0400

    add blogpost model

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from datetime import date
 
 # Create your models here.
 # class MyActivity(models.Model):
@@ -41,3 +42,16 @@ class Note(models.Model):
 
     def get_absolute_url(self):
         return reverse('user_activities', kwargs={'user_id': self.id})
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=250)
+    post_date = date.today()
+    post = models.CharField(max_length=1000)
+    posted_by = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('blogpost_detail', kwargs={'blogpost_id': self.id})
